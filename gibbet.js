edc65c1fc4095d69ebc1e4c817f49bc3b7c07071
Marcin <marcin96semkowski@gmail.com>
2019-12-05 23:35:26 +0100

    Hangman game hide pass #1

--- gibbet.js
+++ gibbet.js
@@ -1,9 +1,22 @@
 var sentence = "Bez pracy nie ma kołaczy";
 
+var length = password1.length;
+
+var password1 = "";
+
+
+for(i=0; i<length; i++){
+    if(password1[i] == ""){
+        password1 = password1 + " ";
+    }else{
+        password1 = password1 + "-";
+    }
+}
+
 
 function writeSentence(){
     
-    document.getElementById("board").innerHTML = sentence;
+    document.getElementById("board").innerHTML = password1;
        
     
 }
