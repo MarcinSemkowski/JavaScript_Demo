45e40fe9bf7a009e2323d99ff0494410c8862558
Marcin <marcin96semkowski@gmail.com>
2019-12-16 20:47:28 +0100

    jQuery JS  #5

---
+++ jQuery/memory.js
@@ -0,0 +1,13 @@
+var cards = ["ciri.png","gerald.png","jaskier.png","jaskier.png","iorweth.png","triss.png","gerald.png",
+            "yen.png","ciri.png","triss.png","yen.png","iorweth.png"];
+
+//alert(cards[4]);
+
+
+//console.log(cards);
+
+
+
+function revealCard(nr){
+    alert(nr);
+}
